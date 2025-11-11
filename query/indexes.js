16384ad7cf57d5197cabd0fdc39526ad5b8158d4
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-11 22:02:17 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,4 +1,4 @@
 //Use this file to define indexes for your queries
-db.restaurants.createIndex({ "address.coord": "2dsphere" });
-db.restaurants.createIndex({ grades.score: 1 });
 db.restaurants.createIndex({ borough: 1 });
+db.restaurants.createIndex({ grades.score: 1 });
+db.restaurants.createIndex({ "address.coord": "2dsphere" });
